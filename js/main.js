79236756ee8974970128aa7814a4f75197915082
Alekosh <aakhosh00@gmail.com>
2024-10-31 02:36:28 +0500

    full done

--- js/main.js
+++ js/main.js
@@ -29,4 +29,25 @@ if (menu && menuBtn) {
 
 // copyrate ==============
 
-// const year = document.querySelector('.current-year').innerHTML = new Date().getFullYear();
+const year = document.querySelector('.current-year').innerHTML = new Date().getFullYear();
+
+// ====================accordionBtns ==============
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
